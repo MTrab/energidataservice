025f5b7294fa4c2c8d24a26bf46f7efec9d0a24c
Malene <malene@trab.dk>
2022-07-16 10:26:24 +0000

    Update workflow and script for releases

--- .github/scripts/update_hacs_manifest.py
+++ .github/scripts/update_hacs_manifest.py
@@ -3,15 +3,22 @@ import json
 import os
 import sys
 
+
 def update_manifest():
     """Update the manifest file."""
     version = "0.0.0"
+    manifest_path = False
     for index, value in enumerate(sys.argv):
         if value in ["--version", "-V"]:
             version = sys.argv[index + 1]
+        if value in ["--path", "-P"]:
+            manifest_path = sys.argv[index + 1]
+
+    if not manifest_path:
+        sys.exit("Missing path to manifest file")
 
     with open(
-        f"{os.getcwd()}/custom_components/energidataservice/manifest.json",
+        f"{os.getcwd()}/{manifest_path.removesuffix('/').removesuffix('/')}/manifest.json",
         encoding="UTF-8",
     ) as manifestfile:
         manifest = json.load(manifestfile)
@@ -29,12 +36,16 @@ def update_manifest():
     new_requirements = []
     for requirement in requirements:
         req = requirement.split("==")[0].lower()
-        new_requirements = [requirement for x in manifest["requirements"] if x.lower().startswith(req)]
-        new_requirements += [x for x in manifest["requirements"] if not x.lower().startswith(req)]
+        new_requirements = [
+            requirement for x in manifest["requirements"] if x.lower().startswith(req)
+        ]
+        new_requirements += [
+            x for x in manifest["requirements"] if not x.lower().startswith(req)
+        ]
         manifest["requirements"] = new_requirements
 
     with open(
-        f"{os.getcwd()}/custom_components/energidataservice/manifest.json",
+        f"{os.getcwd()}/{manifest_path.removesuffix('/').removesuffix('/')}/manifest.json",
         "w",
         encoding="UTF-8",
     ) as manifestfile:
