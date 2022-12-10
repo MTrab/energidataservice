6b95d22d852698690d9cc1ec95a5447206e0bd38
Malene Trab <malene@trab.dk>
2022-12-10 22:51:02 +0000

    Prepare tariffs handler

--- custom_components/energidataservice/tariffs/__init__.py
+++ custom_components/energidataservice/tariffs/__init__.py
@@ -1,4 +1,4 @@
-"""Dynamically load all available forecast providers."""
+"""Dynamically load all available tariff providers."""
 from __future__ import annotations
 
 from collections import namedtuple
@@ -9,18 +9,16 @@ from posixpath import dirname
 
 from genericpath import isdir
 
-from ..const import CURRENCY_LIST, REGIONS
-
 _LOGGER = getLogger(__name__)
 
 
 class Forecast:
-    """Handle forecast modules."""
+    """Handle tariff modules."""
 
     def __init__(self):
-        """Initialize forecast handler."""
+        """Initialize tariff handler."""
 
-        self._forecasts = []
+        self._tariffs = []
         for module in sorted(listdir(f"{dirname(__file__)}")):
             mod_path = f"{dirname(__file__)}/{module}"
             if (
@@ -28,32 +26,15 @@ class Forecast:
                 and not module.endswith("__pycache__")
                 and not mod_path.endswith(".disabled")
             ):
-                Endpoint = namedtuple("Endpoint", "module namespace regions")
+                Endpoint = namedtuple("Endpoint", "module namespace DATA_SCHEME")
                 _LOGGER.debug("Adding module %s", module)
                 api_ns = f".{module}"
                 mod = import_module(api_ns, __name__)
-                con = Endpoint(module, f".forecasts{api_ns}", mod.REGIONS)
+                con = Endpoint(module, f".tariffs{api_ns}", mod.DATA_SCHEME)
 
-                if hasattr(mod, "EXTRA_REGIONS"):
-                    REGIONS.update(mod.EXTRA_REGIONS)
-
-                if hasattr(mod, "EXTRA_CURRENCIES"):
-                    CURRENCY_LIST.update(mod.EXTRA_CURRENCIES)
-
-                self._forecasts.append(con)
+                self._tariffs.append(con)
 
     @property
-    def forecast_endpoints(self) -> list:
-        """Return valid forecast endpoints."""
-        return self._forecasts
-
-    def get_endpoint(self, region: str) -> list:
-        """Get endpoint(s) of a specific zone."""
-        endpoints = []
-
-        for endpoint in self._forecasts:
-            if region in endpoint.regions:
-                ForecastEndpoint = namedtuple("Forecast", "module namespace")
-                endpoints.append(ForecastEndpoint(endpoint.module, endpoint.namespace))
-
-        return endpoints
+    def tariff_endpoints(self) -> list:
+        """Return valid tariff endpoints."""
+        return self._tariffs
