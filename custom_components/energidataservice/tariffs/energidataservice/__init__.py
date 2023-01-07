279f469b3558275dae319de3bc35205a0f4fe3be
Malene Trab <malene@trab.dk>
2023-01-07 20:48:28 +0000

    Fetch and format tariffs

--- custom_components/energidataservice/tariffs/energidataservice/__init__.py
+++ custom_components/energidataservice/tariffs/energidataservice/__init__.py
@@ -2,6 +2,7 @@
 from __future__ import annotations
 import asyncio
 from datetime import datetime, timedelta
+import json
 
 from logging import getLogger
 from typing import Any
@@ -17,6 +18,12 @@ SOURCE_NAME = "Energi Data Service Tariffer"
 
 BASE_URL = "https://api.energidataservice.dk/dataset/DatahubPricelist"
 
+# Source: https://energinet.dk/el/elmarkedet/tariffer/aktuelle-tariffer/
+ADDITIONAL_TARIFFS = {
+    "transmissions_net_tarif": 0.058,
+    "system_tarif": 0.054,
+}
+
 __all__ = ["Connector", "REGIONS", "CHARGEOWNERS"]
 
 
@@ -28,14 +35,20 @@ class Connector:
         self.hass = hass
         self.client = client
         self._chargeowner = chargeowner
-        self._tariff_data = None
+        self._tariffs = {}
         self._result = {}
 
     @property
     def tariffs(self):
         """Return the tariff data."""
-        _LOGGER.debug(self._tariff_data)
-        return self._tariff_data
+        _LOGGER.debug(self._tariffs)
+
+        tariffs = {
+            "additional_tariffs": ADDITIONAL_TARIFFS,
+            "tariffs": self._tariffs,
+        }
+
+        return tariffs
 
     @staticmethod
     def _header() -> dict:
@@ -45,23 +58,43 @@ class Connector:
 
     def _prepare_url(self, url: str) -> str:
         """Prepare and format the URL for the API request."""
-        start_date = (datetime.utcnow() - timedelta(days=1)).strftime("%Y-%m-%d")
-        end_date = (datetime.utcnow() + timedelta(days=10)).strftime("%Y-%m-%d")
+        start_date = (datetime.utcnow()).strftime("%Y-%m-%d")
         start = f"start={str(start_date)}"
-        end = f"end={str(end_date)}"
         limit = "limit=100"
-        objfilter = f"filter=%7B%22Note%22:%5B%22Nettarif C time%22%5D,%22ChargeOwner%22:%5B%22{self._chargeowner}%22%5D%7D"
+        objfilter = f"filter=%7B%22Note%22:%5B%22Nettarif C time%22%5D,%22ChargeOwner%22:%5B%22{self._chargeowner}%22%5D%7D"  # pylint disable=line-too-long
         sort = "sort=ValidFrom%20asc"
 
-        return f"{url}?{start}&{end}&{objfilter}&{sort}&{limit}"
-
-    async def async_get_data(
-        self, url: str, headers: dict | None = None, body: dict | str | None = None
-    ) -> Any:
-        """Make the call to the API."""
-        return None
+        return f"{url}?{start}&{objfilter}&{sort}&{limit}"
 
     async def async_get_tariffs(self):
         """Get tariff from Eloverblik API"""
         headers = self._header()
         url = self._prepare_url(BASE_URL)
+
+        resp = await self.client.get(url, headers=headers)
+
+        if resp.status == 400:
+            _LOGGER.error("API returned error 400, Bad Request!")
+            self._result = {}
+        elif resp.status == 411:
+            _LOGGER.error("API returned error 411, Invalid Request!")
+            self._result = {}
+        elif resp.status == 200:
+            res = await resp.json()
+            self._result = res["records"]
+        else:
+            _LOGGER.error("API returned error %s", str(resp.status))
+            return
+
+        tariff_data = {}
+        for key, val in self._result[0].items():
+            if "Price" in key:
+                hour = str(int("".join(filter(str.isdigit, key))) - 1)
+                if len(hour) == 1:
+                    hour = f"0{hour}"
+
+                tariff_data.update({hour: val})
+        if len(tariff_data) == 24:
+            self._tariffs = tariff_data
+
+        _LOGGER.debug("Tariffs:\n%s", json.dumps(self.tariffs, indent=2, default=str))
