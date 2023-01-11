8ab6a49da86940344db3301c4e456ddab66c371c
Malene Trab <malene@trab.dk>
2023-01-11 19:24:07 +0000

    Removing test file

--- custom_components/energidataservice/tariffs/energidataservice/test.py
+++
@@ -1,9 +0,0 @@
-"""For testing purpose only."""
-
-from chargeowners import CHARGEOWNERS
-
-for chargeowner in CHARGEOWNERS:
-    print(chargeowner)
-
-
-print(CHARGEOWNERS["Hammel Elforsyning Net"])
